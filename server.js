c351c095da1863b01bb12245fe1e8f5c0b2d72f1
Muniyarajbala <muniyarajbalamurugan@gmail.com>
2025-11-26 13:47:03 +0530

    json added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const Razorpay = require("razorpay");
 const mysql = require("mysql2/promise");
 
 const app = express();
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
