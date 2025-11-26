c73d7aa7e673d083cedb70c25ecc1c1791d20700
Muniyarajbala <muniyarajbalamurugan@gmail.com>
2025-11-26 21:19:20 +0530

    seat changed

--- server.js
+++ server.js
@@ -278,12 +278,11 @@ app.post("/book", async (req, res) => {
 app.post("/available-seats", async (req, res) => {
   const { movie_id, date, time_slot_id } = req.body;
 
-  const allSeats = [
-    "A1","A2","A3","A4","A5",
-    "B1","B2","B3","B4","B5",
-    "C1","C2","C3","C4","C5",
+  const allSeats = [    
     "D1","D2","D3","D4","D5",
-    "E1","E2","E3","E4","E5",
+    "C1","C2","C3","C4","C5",
+     "B1","B2","B3","B4","B5",
+     "A1","A2","A3","A4","A5",
   ];
 
   try {
