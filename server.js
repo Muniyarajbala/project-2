bf50998b04e860c7b896a2d238f501cfd2220bfe
Muniyarajbala <muniyarajbalamurugan@gmail.com>
2025-11-26 19:56:19 +0530

    seat deleted

--- server.js
+++ server.js
@@ -279,11 +279,11 @@ app.post("/available-seats", async (req, res) => {
   const { movie_id, date, time_slot_id } = req.body;
 
   const allSeats = [
-    "A1","A2","A3","A4","A5","A6",
-    "B1","B2","B3","B4","B5","B6",
-    "C1","C2","C3","C4","C5","C6",
-    "D1","D2","D3","D4","D5","D6",
-    "E1","E2","E3","E4","E5","E6"
+    "A1","A2","A3","A4","A5",
+    "B1","B2","B3","B4","B5",
+    "C1","C2","C3","C4","C5",
+    "D1","D2","D3","D4","D5",
+    "E1","E2","E3","E4","E5",
   ];
 
   try {
